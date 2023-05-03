c912e12b3a5b59da62bef2788c9d314df80ce992
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-05-02 20:35:47 -0700

    new code

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function newImage(url, left, bottom){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
+function move(image, left, bottom){
+    image.style.position = 'fixed'
 
-newImage('assets/green-character.gif', 100, 250)
+ function moveToCoordinates( left, bottom){
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+}
+
+return{
+    to:moveToCoordinates
+}
+}
+
+move greenCharacter = newImage('assets/green-character.gif'). to(100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
 newImage('assets/pine-tree.png', 450, 350)
@@ -49,3 +58,4 @@ function newInventory(){
 }
 
 const inventory = newInventory()
+
